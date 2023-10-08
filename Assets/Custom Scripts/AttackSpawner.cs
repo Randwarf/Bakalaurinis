11c9dd25b288b8a28d133658a735e315843ac6e4
Randwarf <matas.vaitkevicius@gmail.com>
2023-10-08 21:15:45 +0300

    spawn attack orb

---
+++ Assets/Custom Scripts/AttackSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpawner : MonoBehaviour
+{
+    public Transform[] SpawnLocations;
+    public GameObject attackPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var spawnLocation in SpawnLocations)
+        {
+            var attack = Instantiate(attackPrefab, spawnLocation.position, spawnLocation.rotation);
+            attack.transform.SetParent(transform);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
